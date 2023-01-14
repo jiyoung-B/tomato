723ad1a83a41da06303c6ed25085ee693e28eef6
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2023-01-14 19:59:36 +0900

    create: dummy data

---
+++ src/pages/assets/images/index.ts
@@ -0,0 +1,17 @@
+import babari from"./babari.jpg"
+import chair from"./chair.jpg"
+import socs from"./socs.jpg"
+import bag from"./bag.jpg"
+import user from"./any-icon.png"
+
+
+const contentUrl={
+    babari,
+    chair,
+    socs,
+    bag,
+    user
+
+}
+
+export default contentUrl
